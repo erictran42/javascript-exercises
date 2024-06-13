488d95f1a727c77ea59442049578ae6a21f28fe7
Eric Tran <155506870+erictran42@users.noreply.github.com>
2024-06-13 13:31:37 -0700

    Complete JS exercise 4.
    Use array methods to filter out elements from given string.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function () {
+    let newArray = [];
 
+    for (element of arguments[0]) {
+        let truthValue = 0;
+        for (let i = 1; i < arguments.length; i++) {
+            if (element === arguments[i]) {
+                truthValue += 1;
+            }
+        }
+        if (truthValue == 0) {
+            newArray.push(element);
+        }
+    };
+    return newArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
