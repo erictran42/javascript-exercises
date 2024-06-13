ca6420c9f94cb1be1eeb72108013af54e6ac20df
Eric Tran <155506870+erictran42@users.noreply.github.com>
2024-06-13 14:10:22 -0700

    Complete JS exercises 5, 6, and 7.
    Use Javascript basics to finish basic functions.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (integer1, integer2) {
+    let sum = 0;
+    if (Number.isInteger(integer1) && Number.isInteger(integer2)) {
+        if ((integer1 < 0) || (integer2 < 0)) {
+            return 'ERROR'
+        }
+        if (integer1 > integer2) {
+            small = integer2;
+            big = integer1;
+        } else {
+            small = integer1;
+            big = integer2;
+        }
+        for (let i = small; i <= big; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
