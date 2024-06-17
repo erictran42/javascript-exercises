cf7fb58e83a0ef65e9af75a6daa0585282de4416
Eric Tran <155506870+erictran42@users.noreply.github.com>
2024-06-17 00:25:59 -0700

    Complete JS exercise 11.
    Use array and object methods to find oldest person.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce( (oldest, current) => {
+        let oldestAge = findAge(oldest);
+        age = findAge(current);
+        return oldestAge < age ? current : oldest;
+    });
 };
+
+function findAge(obj) {
+    const currentYear = 2024;
+    if (!obj.yearOfDeath) {
+        obj.yearOfDeath = currentYear;
+    }
+    let age = obj.yearOfDeath - obj.yearOfBirth;
+    return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
