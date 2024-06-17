89aad7599609b7eb4a1a388d9e98179bb9641749
Eric Tran <155506870+erictran42@users.noreply.github.com>
2024-06-16 23:30:41 -0700

    Complete JS exercise 9.
    Use basic JS to check for palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arr = Array.from(string);
+    arr = arr.filter(isAlphaNumeric);
+    let newArr = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+        newArr.push(arr[i]);
+    }
+    return arr.toString().toLowerCase() === newArr.toString().toLowerCase();
+};
 
+function isAlphaNumeric(str) {
+    var code, i, len;
+    for (i = 0, len = str.length; i < len; i++) {
+        code = str.charCodeAt(i);
+        if (!(code > 47 && code < 58) && // numeric (0-9)
+            !(code > 64 && code < 91) && // upper alpha (A-Z)
+            !(code > 96 && code < 123)) { // lower alpha (a-z)
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
