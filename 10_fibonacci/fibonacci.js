856eccd20d66e541685c6365c57054f93162171c
Eric Tran <155506870+erictran42@users.noreply.github.com>
2024-06-16 23:41:47 -0700

    Complete JS exercise 10.
    Create function finding nth term of Fibonacci sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(term) {
+    let previous = 1;
+    let current = 1;
+    for (let i = 2; i < term; i++) {
+        current += previous;
+        previous = current - previous;
+    }
+    if (term === 0 || term === '0') { 
+        current = 0;
+    } else if (term < 0) {
+        current = "OOPS"
+    }
+    return current;
 };
 
 // Do not edit below this line
